fab65afff240c9a88473fa45f4bebcbab31ac364
Marius Kroh <mail@mariuskroh.de>
2019-12-31 20:10:42 +0100

    add api + search form --- off to nye :)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
+const endpoint = "https://raw.githubusercontent.com/mariusKroh/timezones.json/master/timezones.json";
+const timezones = [];
 const secondHand = document.querySelector(".second-hand");
 const minHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
 const container = document.querySelector(".clock-container")
 const date = document.querySelector(".date")
+const searchForm = document.querySelector(".search-form");
+const searchInput = document.querySelector(".search");
+const submitButton = document.querySelector(".make-clock")
+
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => timezones.push(...data))
+  .catch(err => console.log(err));
+
+function findMatches(wordToMatch, timezones) {
+  return timezones.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.utc.match(regex) //|| place.state.match(regex)
 
 
+  })
+}
 
 function pauseTransition(currentValue) {
   if (currentValue === 0) {
@@ -12,8 +29,9 @@ function pauseTransition(currentValue) {
   } else {
     secondHand.classList.add("transition");
   }
-  // needs update not to call eacht second
+  // needs update not to call each second, do we actually need seconds in a world clock?
 }
+
 
 function getUTCTime() {
   const now = new Date()
@@ -21,9 +39,9 @@ function getUTCTime() {
   const utcMinutes = now.getUTCMinutes();
   const utcSeconds = now.getUTCSeconds();
   return {
-    utcHours: utcHours,
-    utcMinutes: utcMinutes,
-    utcSeconds: utcSeconds
+    hours: utcHours,
+    minutes: utcMinutes,
+    seconds: utcSeconds
   }
 }
 
@@ -33,30 +51,35 @@ function setSeconds(seconds) {
   return secondHandRotation
 }
 
-function setDate() {
+function setTime() {
   const rotationOffset = 90;
 
   date.innerHTML = getUTCTime() + "timezone offset is: ";
 
-  const seconds = getUTCTime().utcSeconds;
-
-
+  const seconds = getUTCTime().seconds;
+  const minutes = getUTCTime().minutes;
+  const hours = getUTCTime().hours;
   pauseTransition(seconds);
 
 
 
   secondHand.style.transform = `rotate(${setSeconds(seconds)}deg)`;
 
-  const minutes = getUTCTime().utcMinutes;
   const minutesDegrees = (minutes / 60) * 360 + 90;
   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-  const hours = getUTCTime().utcHours;
   const hoursDegrees = (hours / 12) * 360 + 90;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
   // set Background
   //setBackground(minutes, seconds, hours);
 
+}
+
+function makeClock() {
+  event.preventDefault()
+  const userInput = searchInput.value;
+  console.log(userInput);
 }
 
 // function setBackground(h, s, l) {
@@ -66,4 +89,8 @@ function setDate() {
 //   container.style = `background-color:hsl(${hue},${saturation}%,${light}%)`
 // }
 
-setInterval(setDate, 1000);
+setInterval(setTime, 1000);
+
+//searchForm.addEventListener("change", displayMatches);
+//searchForm.addEventListener("keyup", displayMatches);
+searchForm.addEventListener("submit", makeClock);
